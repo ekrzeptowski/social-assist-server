93f229e16cc74976312cb8793678d255761cb4a3
saj96n <saj96n@gmail.com>
2020-12-20 22:20:20 +0100

    Add pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  apps: [
+    {
+      name: "social-assistant-api",
+      script: "npm -- run start-server",
+
+      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+      // args: 'one two',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      // max_memory_restart: '1G',
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "social-assistant-agenda",
+      script: "npm -- run start-agenda",
+
+      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+      // args: 'one two',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      // max_memory_restart: '1G',
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
